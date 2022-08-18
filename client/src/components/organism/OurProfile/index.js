9e7eda8924e99f28393b449f4820f95f5250f85d
rakshit4013 <rakshit1063@chitkara.edu.in>
2022-08-18 20:34:56 +0530

    structure for leadership and ourprofile

---
+++ client/src/components/organism/OurProfile/index.js
@@ -0,0 +1,28 @@
+import './index.css';
+
+
+const OurProfile=(props)=>{
+
+    // function display()
+    // {
+    //     var li=[];
+    //     var a=
+    //     <div style={{width:"100px",height:"200px",backgroundColor:"cyan"}}>
+    //         Leadership
+    //     </div>
+    //     for(var i=0;i<4;i++)
+    //     {
+
+    //         li.push(a);
+    //     }
+    //     return li;
+    // }
+
+    return (
+    <>
+        ourprofile route
+    </>
+  );
+}
+
+export default OurProfile;
