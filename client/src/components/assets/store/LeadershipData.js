7953609ca29bd71c6f2441e809128941085b5536
rakshit4013 <rakshit1063@chitkara.edu.in>
2022-08-19 09:18:34 +0530

    leadership page made responsive added aboutus navbar non responsive

--- client/src/components/assets/store/LeadershipData.js
+++ client/src/components/assets/store/LeadershipData.js
@@ -3,22 +3,20 @@ import logo from './../../assets/images/navbar_img/navbar_logo_img.svg';
 const LeadershipData={
 
     director:{
-        img:[logo,logo,logo,logo,logo,logo],
-        name:["Rakshit","Samarth","Rahul","Samarth","Samarth","Samarth"],
-        jobTitle:["Developer","Developer","CEO","Job title","Job title","Samarth"]
+        img:[logo,logo,logo,logo,logo,logo,logo,logo,logo],
+        name:["Rakshit","Samarth","Rahul","Samarth","Samarth","Samarth","Samarth","Samarth","Samarth"],
+        jobTitle:["Developer","Developer","CEO","Job title","Job title","Samarth","Samarth","Samarth","Samarth"]
     },
     investor:{
-        img:[logo,logo,logo,logo,logo,logo],
-        name:["Rakshit","Samarth","Rahul","Samarth","Samarth","Samarth"],
-        jobTitle:["Developer","Developer","CEO","Job title","Job title","Samarth"]
+        img:[logo,logo,logo,logo,logo,logo,logo,logo,logo],
+        name:["Rakshit","Samarth","Rahul","Samarth","Samarth","Samarth","Samarth","Samarth","Samarth"],
+        jobTitle:["Developer","Developer","CEO","Job title","Job title","Samarth","Samarth","Samarth","Samarth"]
     },
     juniorLeader:{
-        img:[logo,logo,logo,logo,logo,logo],
-        name:["Rakshit","Samarth","Rahul","Samarth","Samarth","Samarth"],
-        jobTitle:["Developer","Developer","CEO","Job title","Job title","Samarth"]
+        img:[logo,logo,logo,logo,logo,logo,logo,logo,logo],
+        name:["Rakshit","Samarth","Rahul","Samarth","Samarth","Samarth","Samarth","Samarth","Samarth"],
+        jobTitle:["Developer","Developer","CEO","Job title","Job title","Samarth","Samarth","Samarth","Samarth"]
     }
-
-
 }
 
 export default LeadershipData;
