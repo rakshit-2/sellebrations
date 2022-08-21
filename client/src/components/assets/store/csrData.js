02d3fda685e47ed5543f1fb4477f1cf9829c4be1
rakshit4013 <rakshit1063@chitkara.edu.in>
2022-08-21 13:29:19 +0530

    csr page complete not responsive

---
+++ client/src/components/assets/store/csrData.js
@@ -0,0 +1,35 @@
+import logo from './../../assets/images/navbar_img/navbar_logo_img.svg';
+
+const csrData={
+
+    seaction1:
+    {
+        name:"- Mr. Rahul Mehta, CEO",
+        company:"Sellebration Private Limited",
+        img:logo,
+        quote:"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC" 
+    },
+    
+    section2:
+    [
+        {id:0,img:logo},
+        {id:1,img:logo},
+        {id:2,img:logo},
+        {id:3,img:logo},
+        {id:4,img:logo},
+        {id:5,img:logo},
+        {id:6,img:logo},
+        {id:7,img:logo}
+    ],
+
+    section3:
+    {
+        heading:"Bringing the change",
+        id:0,
+        img:logo,
+        quote:"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Contrary to popular belief, Lorem Ipsum is not simply random text.. Contrary to popular belief, Lorem Ipsum is not simply random text.. Contrary to popular belief, Lorem Ipsum is not simply random text. Lorem Ipsum is simply dummy text of the printing and typesetting industry. Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at"
+    },
+
+}
+
+export default csrData;
---
+++ client/src/components/organism/csr/index.js
@@ -0,0 +1,92 @@
+import './index.css';
+import NavbarAboutUs from './../../molecule/NavbarAboutus/index';
+import Carousel from "react-multi-carousel";
+import "./../../../../node_modules/react-multi-carousel/lib/styles.css";
+import items from './../../assets/store/csrData';
+const Csr=(props)=>{
+    const responsive = {
+        desktop: {
+          breakpoint: { max: 3000, min: 1024 },
+          items: 5,
+          slidesToSlide: 1 // optional, default to 1.
+        },
+        tablet: {
+          breakpoint: { max: 1024, min: 464 },
+          items: 3,
+          slidesToSlide: 1 // optional, default to 1.
+        },
+        mobile: {
+          breakpoint: { max: 464, min: 0 },
+          items: 1,
+          slidesToSlide: 1 // optional, default to 1.
+        }
+      };
+    return (
+    <>
+        <div className="csr__outer">
+            <div className="csr__inner">
+                <NavbarAboutUs/>
+                <div className="csr__heading">
+                    <div className="csr__heading__top">
+                        CSR
+                    </div>
+                    <div className="csr__heading__bottom">
+                        CSR at Sellebration
+                    </div>
+                </div>
+                <div className="csr__seaction1">
+                    <div className="csr__seaction1__left">
+                        <div className="csr__section1__left__top">
+                            <span className="csr__collen">“</span>
+                            {items.seaction1.quote}
+                            <span className="csr__collen">”</span>
+                        </div>
+                        <div className="csr__section1__left__bottom">
+                            <span style={{fontStyle:"1.2rem",fontWeight:"700",color:"black"}}>{items.seaction1.name}</span>
+                            <span style={{fontStyle:"1.2rem"}}>{items.seaction1.company}</span>
+                        </div>
+                    </div>
+                    <div className="csr__seaction1__right">
+                        <div className="csr__seaction1__right__inner">
+                            <img src={items.seaction1.img} className="csr__seaction1__right__inner__img"/>
+                        </div>
+                    </div>
+                </div>
+                <div className="csr__seaction2">
+                    <div className="csr__seaction2__heading">
+                        Our Partnerships
+                    </div>
+                    <Carousel responsive={responsive} swipeable={true} draggable={true} containerClass="carousel-container" utoPlaySpeed={1000}
+                    >
+                        {items.section2.map((ele) => {
+                            const {id,img}=ele;
+                            return(
+                                <div key={id} className='csr__seaction2__each'><img src={img} className="csr__crousal__img"/></div>
+                            )
+                        })}
+                    </Carousel>
+                </div>
+                <div className='csr__seaction3'>
+                    <div className="csr__heading">
+                        <div className="csr__heading__bottom">
+                            {items.section3.heading}
+                        </div>
+                    </div>
+                    <div className="csr__seaction3__inner">
+                        <div className='csr__seaction3__inner__left'>
+                            {items.section3.quote} 
+                        </div>
+                        <div className='csr__seaction3__inner__right'>
+                            <div className='csr__seaction3__inner__right__inner'>
+                                <img src={items.section3.img} className="csr__seaction3__inner__right__inner__img"/>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </>
+  );
+}
+
+export default Csr;
